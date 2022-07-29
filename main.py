1f97bf5b2feca31be428e54a84b7699c80fbf261
Skiippa <13388340+Skiippa@users.noreply.github.com>
2022-07-28 21:19:07 -0700

    add key press functionality, add players to the board

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
+from contextlib import nullcontext
 import sys, pygame
 pygame.init()
+
+class Player():
+    
+    def __init__(self, left, top):
+        self.white = 255, 255, 255
+        self.left = left
+        self.top = top
+        pass
+
+    def createLine(self):
+        player = pygame.draw.rect(screen, self.white, pygame.Rect(self.left, self.top, 3, 30))
+        return player
+
 
 size = width, height = 400, 200
 screen = pygame.display.set_mode(size)
 
-
-class Player():
-    
-    def __init__(self):
-        self.white = 255, 255, 255
-        pass
-
-    def createLine(self, left, top):
-        player = pygame.draw.rect(screen, white, pygame.Rect(left, top, 3, 30))
-        return player
-
-player_one = pygame.draw.rect(screen, white, pygame.Rect(0, 0, 3, 30))
-player_two = pygame.draw.rect(screen, white, pygame.Rect(397, 0, 3, 30))
+player_one = Player(0, 0)
+player_two = Player(397, 0)
+player_one.createLine()
+player_two.createLine()
 
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     
+    pressed = pygame.key.get_pressed()
+#https://gamedev.stackexchange.com/questions/198635/how-to-make-objects-move-continuously-while-a-key-is-pressed-in-pygame
+    if pressed[pygame.K_w]:
+        print("W")
 
+    elif pressed[pygame.K_s]:
+        print("S")
 
-    if pygame.KEYDOWN():
+    elif pressed[pygame.K_UP]:
+        print("UP")
+    
+    elif pressed[pygame.K_DOWN]:
+        print("DOWN")
 
 
     pygame.display.flip()
