c8c5c5660ed194d738644b00147f1772bb0d9c61
Skiippa <13388340+Skiippa@users.noreply.github.com>
2022-08-01 11:25:25 -0700

    add 2 players to the window.

--- main.py
+++ main.py
@@ -3,17 +3,78 @@ import sys, pygame
 pygame.init()
 
 class Player():
+    """This class is used to create a player, and all of the player's capabilities."""
     
-    def __init__(self, left, top):
+    def __init__(self, playerX, playerY):
         self.white = 255, 255, 255
-        self.left = left
-        self.top = top
+        self.playerX = playerX
+        self.playerY = playerY
+        self.heldKeys = []
         pass
 
+
     def createLine(self):
-        player = pygame.draw.rect(screen, self.white, pygame.Rect(self.left, self.top, 3, 30))
+        """Draw the player to the screen."""  
+        player = pygame.draw.rect(screen, self.white, pygame.Rect(self.playerX, self.playerY, 3, 30))
+
         return player
 
+
+    def getEvents(self):
+        """Keeps track of key presses/releases, and append/remove them from the list self.heldKeys[]."""
+
+        for i in pygame.event.get():
+            if i.type==pygame.KEYDOWN: #Track key presses here
+                
+                if i.key==pygame.K_w:
+                    self.heldKeys.append('w')
+                
+                if i.key==pygame.K_s:
+                    self.heldKeys.append('s')
+                
+                if i.key==pygame.K_UP:
+                    self.heldKeys.append('UP')
+                
+                if i.key==pygame.K_DOWN:
+                    self.heldKeys.append('DOWN')
+
+                else:
+                    pass
+            elif i.type==pygame.KEYUP: #Track key releases here
+
+                if i.key==pygame.K_w:
+                    self.heldKeys.remove('w')
+                
+                if i.key==pygame.K_s:
+                    self.heldKeys.remove('s')
+                
+                if i.key==pygame.K_UP:
+                    self.heldKeys.remove('UP')
+                
+                if i.key==pygame.K_DOWN:
+                    self.heldKeys.remove('DOWN')
+
+                else:
+                    pass
+            
+            return self.heldKeys
+
+    def checkHeldKeys(self):
+        """Check the keys in heldKeys[] and move accordingly."""
+        for i in self.heldKeys:
+            if 'w' in self.heldKeys:
+                self.playerY -= 1
+            
+            elif 's' in self.heldKeys:
+                self.playerY += 1
+            
+            elif 'UP' in self.heldKeys:
+                self.playerY -= 1
+            
+            elif 'DOWN' in self.heldKeys:
+                self.playerX += 1
+        print(self.heldKeys)
+                
 
 size = width, height = 400, 200
 screen = pygame.display.set_mode(size)
@@ -28,19 +89,8 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     
-    pressed = pygame.key.get_pressed()
-#https://gamedev.stackexchange.com/questions/198635/how-to-make-objects-move-continuously-while-a-key-is-pressed-in-pygame
-    if pressed[pygame.K_w]:
-        print("W")
-
-    elif pressed[pygame.K_s]:
-        print("S")
-
-    elif pressed[pygame.K_UP]:
-        print("UP")
-    
-    elif pressed[pygame.K_DOWN]:
-        print("DOWN")
+    player_one.getEvents()
+    player_one.checkHeldKeys()
 
 
     pygame.display.flip()
