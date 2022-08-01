0408c511ce47dfb99fdb31b32664fdc947efd880
Skiippa <13388340+Skiippa@users.noreply.github.com>
2022-08-01 12:29:51 -0700

    add keytracking

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Player():
         self.playerX = playerX
         self.playerY = playerY
         self.heldKeys = []
+
         pass
 
 
@@ -20,78 +21,77 @@ class Player():
         return player
 
 
-    def getEvents(self):
+    def getEvents(self, i) -> list:
         """Keeps track of key presses/releases, and append/remove them from the list self.heldKeys[]."""
+        
 
-        for i in pygame.event.get():
-            if i.type==pygame.KEYDOWN: #Track key presses here
-                
-                if i.key==pygame.K_w:
-                    self.heldKeys.append('w')
-                
-                if i.key==pygame.K_s:
-                    self.heldKeys.append('s')
-                
-                if i.key==pygame.K_UP:
-                    self.heldKeys.append('UP')
-                
-                if i.key==pygame.K_DOWN:
-                    self.heldKeys.append('DOWN')
-
-                else:
-                    pass
-            elif i.type==pygame.KEYUP: #Track key releases here
-
-                if i.key==pygame.K_w:
-                    self.heldKeys.remove('w')
-                
-                if i.key==pygame.K_s:
-                    self.heldKeys.remove('s')
-                
-                if i.key==pygame.K_UP:
-                    self.heldKeys.remove('UP')
-                
-                if i.key==pygame.K_DOWN:
-                    self.heldKeys.remove('DOWN')
-
-                else:
-                    pass
+        if i.type == pygame.KEYDOWN: #Track key presses here
+            if i.key == pygame.K_w:
+                self.heldKeys.append('w')
             
-            return self.heldKeys
+            if i.key == pygame.K_s:
+                self.heldKeys.append('s')
+            
+            if i.key == pygame.K_UP:
+                self.heldKeys.append('UP')
+            
+            if i.key == pygame.K_DOWN:
+                self.heldKeys.append('DOWN')
+            else:
+                pass
+        elif i.type == pygame.KEYUP: #Track key releases here
+            if i.key == pygame.K_w:
+                self.heldKeys.remove('w')
+            
+            if i.key == pygame.K_s:
+                self.heldKeys.remove('s')
+            
+            if i.key == pygame.K_UP:
+                self.heldKeys.remove('UP')
+            
+            if i.key == pygame.K_DOWN:
+                self.heldKeys.remove('DOWN')
+            else:
+                pass
+        return self.heldKeys
 
-    def checkHeldKeys(self):
-        """Check the keys in heldKeys[] and move accordingly."""
-        for i in self.heldKeys:
-            if 'w' in self.heldKeys:
-                self.playerY -= 1
-            
-            elif 's' in self.heldKeys:
-                self.playerY += 1
-            
-            elif 'UP' in self.heldKeys:
-                self.playerY -= 1
-            
-            elif 'DOWN' in self.heldKeys:
-                self.playerX += 1
-        print(self.heldKeys)
+    def checkHeldKeys(self, events):
+        """Check the keys in keys and move accordingly."""
+
+        keys = self.getEvents(events)
+
+        if keys:
+            for key in keys:
+                if 'w' in keys:
+                    self.playerY -= 1
+
+                elif 's' in keys:
+                    self.playerY += 1
+
+                elif 'UP' in keys:
+                    self.playerY -= 1
+
+                elif 'DOWN' in keys:
+                    self.playerX += 1
+        print(keys)
                 
 
 size = width, height = 400, 200
 screen = pygame.display.set_mode(size)
 
-player_one = Player(0, 0)
-player_two = Player(397, 0)
-player_one.createLine()
-player_two.createLine()
+playerOne = Player(0, 0)
+playerTwo = Player(397, 0)
 
 
-while 1:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    
-    player_one.getEvents()
-    player_one.checkHeldKeys()
 
+        playerOne.checkHeldKeys(event)
+
+    playerOne.createLine()
+    playerTwo.createLine()
+    
 
     pygame.display.flip()
 
