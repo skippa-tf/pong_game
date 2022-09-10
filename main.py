7cc7dc0ab30d6451d442871f495c66d5b242a88c
Skiippa <13388340+Skiippa@users.noreply.github.com>
2022-09-09 17:45:24 -0700

    individualized controls to player number

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ pygame.init()
 class Player():
     """This class is used to create a player, and all of the player's capabilities."""
     
-    def __init__(self, playerX, playerY):
+    def __init__(self, playerX, playerY, playerNumber):
         self.white = 255, 255, 255
         self.playerX = playerX
         self.playerY = playerY
+        self.playerNumber = playerNumber
         self.heldKeys = []
+        self.moveSpeed = 5
 
         pass
 
@@ -24,35 +26,44 @@ class Player():
     def getEvents(self, i) -> list:
         """Keeps track of key presses/releases, and append/remove them from the list self.heldKeys[]."""
         
+        if self.playerNumber == 1:
+            if i.type == pygame.KEYDOWN: #Track key presses here
+                if i.key == pygame.K_w:
+                    self.heldKeys.append('w')
 
-        if i.type == pygame.KEYDOWN: #Track key presses here
-            if i.key == pygame.K_w:
-                self.heldKeys.append('w')
-            
-            if i.key == pygame.K_s:
-                self.heldKeys.append('s')
-            
-            if i.key == pygame.K_UP:
-                self.heldKeys.append('UP')
-            
-            if i.key == pygame.K_DOWN:
-                self.heldKeys.append('DOWN')
-            else:
-                pass
-        elif i.type == pygame.KEYUP: #Track key releases here
-            if i.key == pygame.K_w:
-                self.heldKeys.remove('w')
-            
-            if i.key == pygame.K_s:
-                self.heldKeys.remove('s')
-            
-            if i.key == pygame.K_UP:
-                self.heldKeys.remove('UP')
-            
-            if i.key == pygame.K_DOWN:
-                self.heldKeys.remove('DOWN')
-            else:
-                pass
+                if i.key == pygame.K_s:
+                    self.heldKeys.append('s')
+
+                else:
+                    pass
+            elif i.type == pygame.KEYUP: #Track key releases here
+                if i.key == pygame.K_w:
+                    self.heldKeys.remove('w')
+
+                if i.key == pygame.K_s:
+                    self.heldKeys.remove('s')
+
+                else:
+                    pass
+        else:
+            if i.type == pygame.KEYDOWN: #Track key presses here
+
+                if i.key == pygame.K_UP:
+                    self.heldKeys.append('UP')
+
+                if i.key == pygame.K_DOWN:
+                    self.heldKeys.append('DOWN')
+                else:
+                    pass
+            elif i.type == pygame.KEYUP: #Track key releases here
+
+                if i.key == pygame.K_UP:
+                    self.heldKeys.remove('UP')
+
+                if i.key == pygame.K_DOWN:
+                    self.heldKeys.remove('DOWN')
+                else:
+                    pass
         return self.heldKeys
 
     def checkHeldKeys(self, events):
@@ -61,26 +72,29 @@ class Player():
         keys = self.getEvents(events)
 
         if keys:
-            for key in keys:
-                if 'w' in keys:
-                    self.playerY -= 1
+            if int(self.playerY) <= 196:
+                for key in keys:
+                    if 'w' in keys:
+                        self.playerY -= self.moveSpeed
 
-                elif 's' in keys:
-                    self.playerY += 1
+                    elif 's' in keys:
+                        self.playerY += self.moveSpeed
 
-                elif 'UP' in keys:
-                    self.playerY -= 1
+                    elif 'UP' in keys:
+                        self.playerY -= self.moveSpeed
 
-                elif 'DOWN' in keys:
-                    self.playerX += 1
-        print(keys)
+                    elif 'DOWN' in keys:
+                        self.playerY += self.moveSpeed
+            else:
+                pass
+        print(keys) # for debugging
                 
 
 size = width, height = 400, 200
 screen = pygame.display.set_mode(size)
 
-playerOne = Player(0, 0)
-playerTwo = Player(397, 0)
+playerOne = Player(0, 0, 1)
+playerTwo = Player(397, 0, 2)
 
 
 while True:
