9605fc501ccfd75b5f871c0d3011e3162dba9ef9
skippa-tf <skippa.tf2@gmail.com>
2022-09-30 14:54:05 -0700

    move controls into a dictionary to simplify getEvents.

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ class Player():
         self.playerNumber = playerNumber
         self.heldKeys = []
         self.moveSpeed = height * 0.0125
-
+        self.Controls = {
+            pygame.K_w: 'w',
+            pygame.K_s: 's',
+            pygame.K_UP: 'UP',
+            pygame.K_DOWN: 'DOWN'
+            }
         pass
 
 
@@ -28,44 +33,12 @@ class Player():
     def getEvents(self, i) -> list:
         """Keeps track of key presses/releases, and append/remove them from the list self.heldKeys[]."""
         
-        if self.playerNumber == 1:
-            if i.type == pygame.KEYDOWN: #Track key presses here
-                if i.key == pygame.K_w:
-                    self.heldKeys.append('w')
+        if i.type == pygame.KEYDOWN: #Track key presses here
+            self.heldKeys.append(self.Controls[i.key])
+        
+        elif i.type ==pygame.KEYUP:
+            self.heldKeys.remove(self.Controls[i.key])
 
-                elif i.key == pygame.K_s:
-                    self.heldKeys.append('s')
-
-                else:
-                    pass
-            elif i.type == pygame.KEYUP: #Track key releases here
-                if i.key == pygame.K_w:
-                    self.heldKeys.remove('w')
-
-                elif i.key == pygame.K_s:
-                    self.heldKeys.remove('s')
-
-                else:
-                    pass
-        else:
-            if i.type == pygame.KEYDOWN: #Track key presses here
-
-                if i.key == pygame.K_UP:
-                    self.heldKeys.append('UP')
-
-                elif i.key == pygame.K_DOWN:
-                    self.heldKeys.append('DOWN')
-                else:
-                    pass
-            elif i.type == pygame.KEYUP: #Track key releases here
-
-                if i.key == pygame.K_UP:
-                    self.heldKeys.remove('UP')
-
-                elif i.key == pygame.K_DOWN:
-                    self.heldKeys.remove('DOWN')
-                else:
-                    pass
         return self.heldKeys
 
     def checkHeldKeys(self, events):
@@ -74,33 +47,36 @@ class Player():
         keys = self.getEvents(events)
 
         if keys:
-            if 0 <= self.playerY < int(size[1] - self.playerHeight):
-                #for key in keys:
+            for key in keys:
                 if 'w' or 'UP' in keys:
                     self.movePaddle('up')
+                    
                 elif 's' or 'DOWN' in keys:
                     self.movePaddle('down')
 
         else:
             pass
-        print(keys) # for debugging
+
+        print(keys)
+        print(self.playerY) # for debugging
     
     def movePaddle(self, direction):
         """Moves the paddle a certain direction."""
 
-        if direction == 'up':
+        if direction == 'up' and self.playerY > 0:
             self.playerY -= self.moveSpeed
 
-        else:
+        elif direction == 'down' and self.playerY < (height - self.playerHeight):
             self.playerY += self.moveSpeed
 
                 
 
 size = width, height = 1920, 1080
 screen = pygame.display.set_mode(size)
+playerTwoX = width - width * 0.0075
 
 playerOne = Player(0, 0, 1)
-playerTwo = Player(width - width * 0.0075, 0, 2)
+playerTwo = Player(playerTwoX, 0, 2)
 
 
 while True:
